c49a52f06bb66fdef58067abe41a5a695f154ba1
J-Uhero <juho.herranen@gmail.com>
2021-11-28 17:26:00 +0200

    tips_repositories.py luotu lukuvinkkejä koskevia tietokantakyselyitä varten

---
+++ src/repositories/tips_repository.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from database_connection import database_connection as default_database_connection
+
+class TipsRepository:
+    def __init__(self, database_connection=default_database_connection):
+        self._connection = database_connection
+
+    def create_tip(self, title):
+        try:
+            sql = "INSERT INTO Tips (title, ) VALUES (?, ?);"
+            time = datetime()
+            cursor = self._connection.cursor()
+            cursor.execute(sql, [title, time])
+            self._connection.commit()
+            return True
+        except:
+            return False
+
+tips_repository = TipsRepository()
