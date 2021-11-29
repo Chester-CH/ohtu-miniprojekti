91651bee30f5ad73f375be9aabba65366555a771
J-Uhero <juho.herranen@gmail.com>
2021-11-30 00:59:38 +0200

    testit tips_repository-luokalle

---
+++ src/tests/tips_repository_test.py
@@ -0,0 +1,24 @@
+from database_connection import create_database_connection, TEST_DATABASE_NAME
+from initialize_database import initialize_database
+from repositories.tips_repository import TipsRepository
+import unittest
+
+class TestTipsRepository(unittest.TestCase):
+    def setUp(self):
+        self.connection = create_database_connection(TEST_DATABASE_NAME)
+        initialize_database(self.connection)
+        self.tips_repository = TipsRepository(self.connection)
+
+    def test_created_tip_gives_boolean_true(self):
+        boolean = self.tips_repository.create_tip("Some_Title")
+        self.assertEqual(boolean, True)
+    
+    def test_create_tip_gives_the_right_title(self):
+        title = "Some_Title"
+        self.tips_repository.create_tip(title)
+
+        sql = "SELECT title FROM Tips;"
+        cursor = self.connection.cursor()
+        answer = cursor.execute(sql).fetchone()
+        print(answer[0])
+        self.assertEqual(answer[0], title)
