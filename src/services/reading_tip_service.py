478d0b8557734765ead3a4f32c5bc5d0e600b546
Makakoodi <mar.merilainen@gmail.com>
2021-11-29 18:17:42 +0200

    Kommenttien lisäys.

--- src/services/reading_tip_service.py
+++ src/services/reading_tip_service.py
@@ -1,10 +1,13 @@
 from entities.reading_tip import ReadingTip
 from repositories.tips_repository import (tips_repository as default_tips_repository)
 
+
+#konstruktori luo sovelluslogiikasta vastaavan palvelun
 def __init__(self, tips_repository=default_tips_repository     
     ):
     self._tips_repository = tips_repository
 
+#Luo uuden lukuvinkin
 def create_reading_tip(content):    
 
     return self.tips_repository.create_tip(reading_tip.title)
