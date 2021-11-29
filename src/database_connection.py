800ecbd874ecf72bcf4a988b2b08fb40050fd7b0
J-Uhero <juho.herranen@gmail.com>
2021-11-30 00:51:26 +0200

    database_connection päivitetty

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,7 +1,13 @@
 import sqlite3
 import os
 
-dirname = os.path.dirname(__file__)
 DATABASE_NAME = "reading_tips_database.db"
-database_connection = sqlite3.connect(os.path.join(dirname, "..", "data", DATABASE_NAME))
-database_connection.row_factory = sqlite3.Row
+TEST_DATABASE_NAME = "test_database.db"
+
+def create_database_connection(database_name=DATABASE_NAME):
+    dirname = os.path.dirname(__file__)
+    database_connection = sqlite3.connect(os.path.join(dirname, "..", "data", database_name))
+    database_connection.row_factory = sqlite3.Row
+    return database_connection
+
+database_connection = create_database_connection()
