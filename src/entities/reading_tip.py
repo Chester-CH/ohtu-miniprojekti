cf9a298e7d9fb6e39088af4d0e95e461a0ecfd4f
zhishangchen1995@gmail.com <zhishangchen@hotmail.com>
2021-12-06 14:12:53 +0200

    id

--- src/entities/reading_tip.py
+++ src/entities/reading_tip.py
@@ -5,17 +5,23 @@ class ReadingTip:
         title: String, which is the name of the reading tips
     """
 
-    def __init__(self, title):
+    def __init__(self, title, tip_id):
         """Class constructor, which creates a new reading tip.
         Args:
             title: A string, which is the title of the reading tip
         """
         self._title = title
+        self._tip_id = tip_id
 
     @property
     def title(self):
         return self._title
 
+    @property
+    def tip_id(self):
+        return self._tip_id
+
     @title.setter
-    def title(self, title):
+    def title(self, title, tip_id):
         self._title = title
+        self._tip_id = tip_id
