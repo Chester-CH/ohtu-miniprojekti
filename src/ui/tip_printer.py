e0f7167c39810131b4a0cb270cfd9bafad79278b
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2021-12-14 16:37:37 +0200

    Lisää tipsien kenttien tulostaminen

--- src/ui/tip_printer.py
+++ src/ui/tip_printer.py
@@ -1,20 +1,44 @@
 from entities.tip_types import TipTypes
 
 
-def _get_book_description(book_tip):
+def _value_string(value):
+    if value:
+        return str(value)
     return ""
+
+
+def _add_description(string_description, tip):
+    if tip['description']:
+        string_description += f"\nKuvaus: {_value_string(tip['description'])}"
+    return string_description
+
+
+def _get_book_description(book_tip):
+    string_description = "\n" + \
+        f"Kirjailija: {_value_string(book_tip['author'])}, " + \
+        f"ISBN: {_value_string(book_tip['isbn'])}"
+    return _add_description(string_description, book_tip)
 
 
 def _get_blogpost_description(blog_tip):
-    return ""
+    string_description = "\n" + \
+        f"Tekijä: {_value_string(blog_tip['author'])}, " + \
+        f"URL: {_value_string(blog_tip['url'])}"
+    return _add_description(string_description, blog_tip)
 
 
 def _get_video_description(video_tip):
-    return ""
+    string_description = "\n" + \
+        f"URL: {_value_string(video_tip['url'])}"
+    return _add_description(string_description, video_tip)
 
 
 def _get_podcast_description(podcast_tip):
-    return ""
+    string_description = "\n" + \
+        f"Podcastin nimi: {_value_string(podcast_tip['name'])}, " + \
+        f"Tekijä: {_value_string(podcast_tip['author'])}\n" + \
+        f"URL: {_value_string(podcast_tip['url'])}"
+    return _add_description(string_description, podcast_tip)
 
 
 class TipPrinter:
@@ -37,5 +61,5 @@ class TipPrinter:
     def get_description(tip):
         """ Returns a string description of the contents of this tip. """
         description = f"{tip['title']}, {TipPrinter.TIP_TYPE_IN_FINNISH[tip['tip_type']]}" + \
-            TipPrinter.TIP_TYPE_PRINTERS[tip['tip_type']](tip)
+            TipPrinter.TIP_TYPE_PRINTERS[tip['tip_type']](tip) + "\n"
         return description
